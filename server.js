510055db187c4d1b520e20ee5437674e608c371c
nyari769mit <nyari769@mit.edu>
2026-04-02 10:32:16 -0400

    Fix static file order

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const whiteRoom = require("./engine");
 
 const app = express();
-app.use(express.static(__dirname));
 app.use(express.json());
+app.use(express.static(__dirname));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
